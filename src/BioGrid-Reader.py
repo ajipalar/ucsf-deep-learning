f2082664967e1c6ef16c5913055c253136d01923
ajipalar <aji.palar@ucsf.edu>
2021-05-11 12:06:56 -0700

    summarize data

--- src/BioGrid-Reader.py
+++ src/BioGrid-Reader.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import matplotlib as plt
 
+dtypes = { }
 d = pd.read_csv("../data/BIOGRID-ALL-4.4.197.tab3.txt", delimiter="\t")
-print(d.head)
+print(d.columns)
+print(d.head())
 
